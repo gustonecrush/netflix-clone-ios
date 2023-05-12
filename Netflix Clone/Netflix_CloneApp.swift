914fa5d1fccb5d8261a6d0e800bd8e266e98663c
gustonecrush <codeboxezz@gmail.com>
2023-05-12 13:29:08 +0700

    initialized project ios

---
+++ Netflix Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix Clone
+//
+//  Created by A Farhan Agustiansyah on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
