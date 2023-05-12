9e13249454e01feeb0eed0de3adf31088beb3582
gustonecrush <codeboxezz@gmail.com>
2023-05-12 13:45:22 +0700

    added package SDWebImageSwiftUI

---
+++ Netflix Clone/Views/SplashScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreen.swift
+//  Netflix Clone
+//
+//  Created by A Farhan Agustiansyah on 12/05/23.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
