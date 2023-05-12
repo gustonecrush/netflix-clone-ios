6bf66055cbb2a7ef07deac82c00a8142d3548b2d
gustonecrush <codeboxezz@gmail.com>
2023-05-12 17:58:36 +0700

    created bottom tab navigation

---
+++ Netflix Clone/Views/Components/TabBar.swift
@@ -0,0 +1,138 @@
+//
+//  TabBar.swift
+//  Netflix Clone
+//
+//  Created by A Farhan Agustiansyah on 12/05/23.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    
+    @Binding var selectedTab: String
+    
+    // Storing Each Tab MidPoints to animate it in future ...
+    @State var tabPoints: [CGFloat] = []
+    
+    var body: some View {
+        
+        HStack(spacing: 0, content: {
+            
+            // Tab Bar Buttons
+            TabBarButton(image: "circle.grid.2x2", selectedTab: $selectedTab, tabPoints: $tabPoints)
+            
+            TabBarButton(image: "magnifyingglass", selectedTab: $selectedTab, tabPoints: $tabPoints)
+            
+            TabBarButton(image: "play.tv", selectedTab: $selectedTab, tabPoints: $tabPoints)
+            
+            TabBarButton(image: "arrow.down.circle", selectedTab: $selectedTab, tabPoints: $tabPoints)
+            
+            TabBarButton(image: "person", selectedTab: $selectedTab, tabPoints: $tabPoints)
+            
+        })
+            .padding()
+            .background(Color("BGNavigation").clipShape(TabCurve(tabPoint: getCurvePoint() - 15)))
+            .overlay (
+                Circle()
+                    .fill(Color("Red"))
+                    .frame(width: 10, height: 10)
+                    .offset(x: getCurvePoint() - 20)
+                
+                ,alignment: .bottomLeading
+            )
+            .cornerRadius(30)
+            .padding(.horizontal)
+        
+    }
+    
+    // extracting point ...
+    func getCurvePoint() -> CGFloat {
+        
+        // if tabpoint is empty ...
+        if tabPoints.isEmpty {
+            return 10
+        } else {
+            switch selectedTab {
+            case "circle.grid.2x2":
+                return tabPoints[0]
+            case "magnifyingglass":
+                return tabPoints[1]
+            case "play.tv":
+                return tabPoints[2]
+            case "arrow.down.circle":
+                return tabPoints[3]
+            default:
+                return tabPoints[4]
+            }
+        }
+        
+    }
+    
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
+
+
+struct TabBarButton: View {
+    
+    let image: String
+    @Binding var selectedTab: String
+    @Binding var tabPoints: [CGFloat]
+    
+    var body: some View {
+        
+        // To getting mid point of each button fur curve animation
+        GeometryReader { reader -> AnyView in
+            
+            // extracting midpoint and storing ...
+            let midX = reader.frame(in: .global).midX
+            
+            DispatchQueue.main.async {
+                
+                // avoiding junk data ...
+                if tabPoints.count <= 4 {
+                    tabPoints.append(midX)
+                }
+                
+            }
+            
+            return AnyView (
+            
+                Button(action: {
+                    
+                    // changing tab
+                    // spring animation ...
+                    withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.5, blendDuration: 0.5)) {
+                        
+                        selectedTab = image
+                        
+                    }
+                    
+                }, label: {
+                    
+                    // Filling the color if it's selected ...
+                    
+                    Image(systemName: "\(image)\(selectedTab == image && image != "magnifyingglass" ? ".fill" : "")")
+                        .font(.system(size: 25, weight: .semibold))
+                        .foregroundColor(selectedTab == image ? Color("Red") :  Color("Gray"))
+                    // Lifting View ...
+                    // If its selected ...
+                        .offset(y: selectedTab == image ? -10 : 0)
+                    
+                })
+                // Max frame ...
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+            )
+            
+        }
+        // Max height
+        .frame(height: 50)
+        
+    }
+    
+}
--- Netflix Clone/Views/HomeScreen.swift
+++ Netflix Clone/Views/HomeScreen.swift
@@ -8,62 +8,20 @@
 import SwiftUI
 
 struct HomeScreen: View {
+    
+    @State var selectedTab = "circle.grid.2x2"
+    
     var body: some View {
         
-        VStack {
+        ZStack(alignment: .bottom, content: {
             
-            HStack(spacing: 12) {
-                
-                Image("logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 100, height: 30)
-                
-                Spacer()
-                
-                Button("Help") {
-                    
-                }
-                
-                Button("Privacy") {
-                    
-                }
-                
-            }
-            .font(.system(size: 16, weight: .bold))
-            .foregroundColor(.primary)
+            Color("BG")
+                .ignoresSafeArea()
             
-            Image("onboard")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-                .padding(.top, 30)
+            // MARK: Tab Bar
+            TabBar(selectedTab: $selectedTab)
             
-            Text("Watch on any device")
-                .font(.title.bold())
-                .padding(.top, 25)
-            
-            Text("Stream on your phone, tablet, \nlaptop, TV.")
-                .font(.title2)
-                .multilineTextAlignment(.center)
-                .padding(.top, 18)
-            
-            Spacer()
-            
-            Button {
-                
-            } label: {
-                
-                Text("SIGN IN")
-                    .foregroundColor(.white)
-                    .padding(.vertical, 12)
-                    .frame(maxWidth: .infinity)
-                    .background(Color("Red"))
-                
-            }
-            
-        }
-        .padding()
+        })
         
     }
 }
